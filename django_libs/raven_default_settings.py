2079b4ca04c2553fa0c5557adf7f9a575935e8d1
Martin Brochhaus <mbrochh@gmail.com>
2014-07-27 13:04:37 +0200

    Changed regex for RAVEN_IGNORABLE_USER_AGENTS

--- django_libs/raven_default_settings.py
+++ django_libs/raven_default_settings.py
@@ -1,13 +1,13 @@
 """Default settings for the CustomRaven404CatchMiddleware"""
 RAVEN_IGNORABLE_USER_AGENTS = [
-    r'^.*AhrefsBot.*$',
-    r'^.*EasouSpider.*$',
-    r'^.*FacebookBot.*$',
-    r'^.*Feedfetcher-Google.*$',
-    r'^.*Googlebot.*$',
-    r'^.*Test Certificate Info.*$',
-    r'^.*Twitterbot.*$',
-    r'^.*Yahoo! Slurp.*$',
-    r'^.*YandexBot.*$',
-    r'^.*bingbot.*$',
+    r'AhrefsBot',
+    r'EasouSpider',
+    r'FacebookBot',
+    r'Feedfetcher-Google',
+    r'Googlebot',
+    r'Test Certificate Info',
+    r'Twitterbot',
+    r'Yahoo! Slurp',
+    r'YandexBot',
+    r'bingbot',
 ]

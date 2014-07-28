850c2c1f61dcab519bb492b034c800617c581ca5
Martin Brochhaus <mbrochh@gmail.com>
2014-07-28 13:18:12 +0200

    Small fixes for the test suit

--- django_libs/tests/test_app/urls.py
+++ django_libs/tests/test_app/urls.py
@@ -1,5 +1,5 @@
 """URLs for the test app."""
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.http import HttpResponse
 from django.views.generic import View
 

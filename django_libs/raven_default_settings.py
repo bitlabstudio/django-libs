761068070c5a6a5f680403b62acfa0600c103593
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-08-31 16:02:36 +0200

    Added utils module

--- django_libs/raven_default_settings.py
+++ django_libs/raven_default_settings.py
@@ -1,6 +1,10 @@
 """Default settings for the CustomRaven404CatchMiddleware"""
 from . import default_settings
 
+raise DeprecationWarning(
+    'This setting is deprecated.'
+    ' Please import from django_libs.default_settings instead.')
+
 # NOTE: kept for backwards compatibility. Default setting should always be
 # defined in ``default_settings.py``
 RAVEN_IGNORABLE_USER_AGENTS = \
--- django_libs/utils_email.py
+++ django_libs/utils_email.py
@@ -1,36 +1,4 @@
-"""Utility functions for sending emails."""
-from django.template import RequestContext
-from django.template.loader import render_to_string
+"""Kept for backwards compatibility. Please add everything to the path below."""
+from .utils.email import *  # NOQA
 
-import mailer
-
-from .utils import html_to_plain_text
-
-
-def send_email(request, extra_context, subject_template, body_template,
-               from_email, recipients, priority="medium"):
-    """
-    Sends an email based on templates for subject and body.
-
-    :param request: The current request instance.
-    :param extra_context: A dictionary of items that should be added to the
-        templates' contexts.
-    :param subject_template: A string representing the path to the template of
-        of the email's subject.
-    :param body_template: A string representing the path to the template of
-        the email's body.
-    :param from_email: String that represents the sender of the email.
-    :param recipients: A list of tuples of recipients. The tuples are similar
-        to the ADMINS setting.
-
-    """
-    if request:
-        context = RequestContext(request, extra_context)
-    else:
-        context = extra_context
-    subject = render_to_string(subject_template, context)
-    subject = ''.join(subject.splitlines())
-    message_html = render_to_string(body_template, context)
-    message_plaintext = html_to_plain_text(message_html)
-    mailer.send_html_mail(subject, message_plaintext, message_html, from_email,
-                          recipients, priority=priority)
+raise DeprecationWarning('Please import from django_libs.utils.email instead.')

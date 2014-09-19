eaecc16346566df1a678899d1a314511f103f486
Martin Brochhaus <mbrochh@gmail.com>
2014-09-20 01:01:01 +0800

    Added django_settings.py with IGNORABLE_404_URLS

---
+++ django_libs/settings/django_settings.py
@@ -0,0 +1,9 @@
+"""Useful defualt settings that you might want to use in all your projects."""
+import re
+
+
+IGNORABLE_404_URLS = [
+    re.compile(r'\.*$'),
+    re.compile(r'^/media/'),
+    re.compile(r'^/static/'),
+]

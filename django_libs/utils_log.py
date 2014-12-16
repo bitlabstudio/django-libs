af7952f0650ad410c08f8cc8c768f11b1df097e4
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-12-16 13:00:31 +0100

    Fixed import

--- django_libs/utils_log.py
+++ django_libs/utils_log.py
@@ -1,7 +1,7 @@
 """Kept for backwards compatibility. Please add everything to the path below."""
 import warnings
 
-from .utils.email import *  # NOQA
+from .utils.log import *  # NOQA
 
 warnings.warn('Please import from django_libs.utils.log instead.',
               DeprecationWarning)

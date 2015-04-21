a0eb5bc04afee4f02b8ea09fd245be61c397f510
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-04-21 19:08:58 +0200

    fixed cookie update view

--- django_libs/views.py
+++ django_libs/views.py
@@ -200,8 +200,8 @@ class UpdateCookieAJAXView(View):
         if not (request.is_ajax() and request.method == 'POST'):
             return HttpResponseForbidden()
         response = HttpResponse('done')
-        days = request.POST.get('cookie_days', 100)
-        date = datetime.datetime.utcnow() + datetime.timedelta(days=int(days))
+        days = int(request.POST.get('cookie_days', 100))
+        date = datetime.datetime.utcnow() + datetime.timedelta(days=days)
         expires = datetime.datetime.strftime(date, "%a, %d-%b-%Y %H:%M:%S GMT")
         response.set_cookie(
             request.POST['cookie_key'],

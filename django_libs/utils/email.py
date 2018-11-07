8c44a0851e3be564a100df50d257c1ce5b30dc25
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2018-11-07 22:58:26 +0100

    Added cc/bcc support to email sendings

--- django_libs/utils/email.py
+++ django_libs/utils/email.py
@@ -7,6 +7,7 @@ from django.utils.encoding import force_text
 
 try:
     import mailer
+    from mailer.models import make_message
 except ImportError:  # pragma: nocover
     pass
 
@@ -62,22 +63,30 @@ def send_email(request, context, subject_template, body_template,
     message_html = render_to_string(template_name=body_template,
                                     context=context, request=request)
     message_plaintext = html_to_plain_text(message_html)
+    subject = force_text(subject)
+    message = force_text(message_plaintext)
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=message,
+        from_email=from_email,
+        to=recipients,
+        cc=cc,
+        bcc=bcc,
+        headers=headers,
+        reply_to=[reply_to],
+    )
+    email.attach_alternative(message_html, "text/html")
     if settings.EMAIL_BACKEND == 'mailer.backend.DbBackend':
-        mailer.send_html_mail(
-            subject, message_plaintext, message_html, from_email, recipients,
-            priority=priority, headers=headers)
-    else:
-        subject = force_text(subject)
-        message = force_text(message_plaintext)
-        email = EmailMultiAlternatives(
+        # We customize `mailer.send_html_mail` to enable CC and BCC
+        priority = mailer.get_priority(priority)
+        msg = make_message(
             subject=subject,
             body=message,
             from_email=from_email,
             to=recipients,
-            cc=cc,
-            bcc=bcc,
-            headers=headers,
-            reply_to=[reply_to],
+            priority=priority,
         )
-        email.attach_alternative(message_html, "text/html")
+        msg.email = email
+        msg.save()
+    else:
         email.send()
